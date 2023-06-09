5a34890a8fb917d1decd0cfd11e3811a792a55cc
GitHub Action <github-actions[bot]@users.noreply.github.com>
2023-06-09 00:14:53 +0000

    GitHub Action: Raycast version update: 1.53.1

--- flake.nix
+++ flake.nix
@@ -25,8 +25,8 @@
               raycast = super.raycast.overrideAttrs (old: {
                 pname = "raycast";
                 src = builtins.fetchurl {
-                  url = "https://archive.org/download/raycast/raycast-1.53.0.dmg";
-                  sha256 = "sha256-IkLbfuzkgO/E8U8PjZ6NQ28WEVeBumKYbbKpK/LPwNY=";
+                  url = "https://archive.org/download/raycast/raycast-1.53.1.dmg";
+                  sha256 = "sha256-Fb5CtGwkwsvmhcQQmXGH5qXOG4xesOBaxEy3taQUdJo=";
                 };
               });
             })
